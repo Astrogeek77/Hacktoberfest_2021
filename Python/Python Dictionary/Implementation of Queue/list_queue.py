45931cccdb043161eed420628e38d2e41e70f36a
Chris Lopes <54064653+chickenCHRISpy@users.noreply.github.com>
2021-10-05 00:15:22 +0530

    Create list_queue.py
    This is an implementation of Queue data structure in python. It is done using a list.

---
+++ Python/Python Dictionary/Implementation of Queue/list_queue.py
@@ -0,0 +1,11 @@
+# implementing Queue using List :
+q=[]
+q.append(10)
+q.append(100)
+q.append(1000)
+q.append(10000)
+print("Initial Queue is:",q)
+print(q.pop(0))
+print(q.pop(0))
+print(q.pop(0))
+print("After Removing elements:",q)
