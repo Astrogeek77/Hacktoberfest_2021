3a9bd37b1ebe908be3d414b11ad3ac3319fe711c
Abhishek kalra <abhishekkalra560@gmail.com>
2021-10-04 17:19:15 +0530

    calculator project added

---
+++ JavaScript/calculator/calculator.js
@@ -0,0 +1,14 @@
+function eva() {
+    var  a=""
+    var  b=""
+      a=document.getElementById("ok").value;
+       b=eval(a);
+        document.getElementById("ok").value = b;
+    
+    
+    
+    }
+    function c(){
+      var c=document.getElementById("ok").value = ""
+    }
+    
