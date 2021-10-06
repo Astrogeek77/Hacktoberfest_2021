bee8ea26a0d686e5aba354f1da81bd447a46d563
Shubham Mallya <91719093+shubhamSM1298@users.noreply.github.com>
2021-10-06 13:09:55 +0530

    FibonacciSeries in Java
    done

---
+++ Java/FibonacciSeries.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+import java.io.*;
+import java.util.*;
+    class Main
+    {
+        public static void main(String []args)
+        {
+            Scanner q = new Scanner(System.in);
+            int p=q.nextInt();
+            int a=1,b=1;
+            System.out.print(a+" "+b+" ");
+            for(int i=2;i<p;i++)
+            {
+                int sum=a+b;
+                    System.out.print(sum+" ");
+
+                a=b;
+                b=sum;
+
+            }
+
+
+        }
+
+
+    }
