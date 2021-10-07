3b2506381e7da939f102c53fd951702e5f93cf93
komal kumari <komalk.8009@gmail.com>
2021-10-07 09:07:20 +0530

    added Fibonacci Series

---
+++ CPP/FibonacciSeries.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void fib(int n){
+    int t1 = 0;
+    int t2 = 1;
+    int nextTerm;
+
+    for(int i=0; i<=n ; i++){
+        cout<<t1<<endl;
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    fib(n);
+
+
+}
